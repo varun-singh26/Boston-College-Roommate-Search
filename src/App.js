9749663b4178abb254a85cf9082d6079e2054685
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2024-12-28 12:34:09 -0800

    Added comments to App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,22 @@ import Footer from "./components/Footer.jsx";
 import css from './App.css';
 
 
-//TODO: Will This Layout Work?
+//Web App LAYOUT so far:
+
+//Path "/" (root):
+//Right now the LandingSplash component only contains the image of Gasson Hall, the On-Campus and Off-Campus button, and the search for a posting form (broken up into two forms).
+
+//Path "/postings":
+//Right now the two ways of getting to "/postings" are by submitting the search form from the root path,
+//(doing so updates listingLocation and formData in the SearchContext), or by clicking the "Postings" link in the Navbar. 
+//However if you submit the search form but navigate back to the root page, the SearchContext becomes reinitalized again (to 0/empty).
+
+//Path "/detailView":
+//When rendering the detailView for a specific posting, we can add query parameters (of the posting that was clicked, such as ID) to the url.
+//Then in detailView.jsx, the query parameters can be parsed and used to render the appropriate posting in the database
+
+
+
 function RoutesWrapper({ data }) {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
@@ -28,6 +43,34 @@ function RoutesWrapper({ data }) {
           element = {
             <>
               <Posts />
+            </>
+          }
+        />
+        <Route
+          path ="/detailView"
+          element = {
+            <>
+            </>
+          }
+        />
+        <Route
+          path ="/signIn"
+          element = {
+            <>
+            </>
+          }
+        />
+        <Route
+          path ="/signUp"
+          element = {
+            <>
+            </>
+          }
+        />
+        <Route
+          path ="/about"
+          element = {
+            <>
             </>
           }
         />
