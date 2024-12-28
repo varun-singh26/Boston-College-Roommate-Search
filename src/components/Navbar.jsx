3deb99cad0ec38398b61102cd3bfc14a3fe19ee0
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2024-12-28 14:04:18 -0800

    Changed internal links in Navbar.jsx to use <Link> instead of <a>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+//We want to use Link for internal links of our web app to avoid full page reloads when they are clicked (to preserve any contexts)
+import { Link } from "react-router-dom";
 import css from "../styles/Navbar.module.css" 
 
 const Navbar = () => {
 
-    const location = useLocation(); //How does this work?
-    const isActive = (path) => location.pathname === path; /*A function */
+    const location = useLocation(); //provides access to the current location object (ie. location.pathname, location.search)
+                                    //useLocation dynamically updates whenever the route changes (which changes when the path changes)
+                                    //Allows us to update this component based on the current route
+    const isActive = (path) => location.pathname === path; 
 
-    //TODO: Why isn't isActive used within the Houding Calendar or Resources links?
     return (
         <header>
             <div id="nav" className={css.navContainer}>
                 <nav className= {css.container}>
                     <h2 className= {css.bcrsHeader}>
-                        <a href="/">
+                        <Link to="/">
                             <span className= {css.goldLogo}>BC</span>
                             <span className= {css.whiteLogo}>RS</span>
-                        </a>
+                        </Link>
                     </h2>
                     <div className={css.links}>
                         <p className={isActive("/") ? css.activeLink : ""}>
-                            <a href ="/" className={css.homeLink}>Home</a>
+                            <Link to="/" className={css.homeLink}>Home</Link>
                         </p>
                         <p className={isActive("/postings") ? css.activeLink : ""}>
-                            <a href = "/postings" className={css.postingsLink}>Postings</a>
+                            <Link to= "/postings" className={css.postingsLink}>Postings</Link>
                         </p>
 
                         <p>
@@ -48,7 +51,7 @@ const Navbar = () => {
                         </p>
                     </div>
                     <div className= {css.signInContainer}>
-                        <a href="/signin" className={css.signInLink}>Sign In</a>
+                        <Link to="/signin" className={css.signInLink}>Sign In</Link>
                         <img
                             src="https://cdn.glitch.global/c7d70598-61bb-4c55-ac66-58662df41931/Profile%20Icon.png?v=1732691728436"
                             alt="Profile Icon"
