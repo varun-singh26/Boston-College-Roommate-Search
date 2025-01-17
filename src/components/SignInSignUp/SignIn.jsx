f8ff473345acd5b05d9486225af44cc385d349cb
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2025-01-17 17:22:02 -0500

    Added sign-in with Google

--- src/components/SignInSignUp/SignIn.jsx
+++ src/components/SignInSignUp/SignIn.jsx
@@ -68,19 +68,34 @@ const SignIn = () => {
     }
   };
 
-  const onGoogleSignIn = (e) => {
+  const onGoogleSignIn = async (e) => {
     e.preventDefault()
     if(!loading) {
       setLoading(true);
-      doSignInWithGoogle().catch(err => {
-        setLoading(false)
-      });    //could be an error with users signing in w Google popup. If so, we need to catch this error and set isSigningIn to false (why?)
+      try {
+        const result = await doSignInWithGoogle();
+  
+        if (result) { //What type of object is result?
+          console.log("Google Sign-In successful:", result.user);
+          //CurrentUser and userLoggedIn are set to the logged in user and true, respectively via auth/index.jsx
+          //setCurrentUser(result.user);
+          //setUserLoggedIn(true);
+          console.log("current user: ", result.user); //runs immediately, here so we can see the updated state in the console
+          console.log("user logged in? ", true); //Here for same reason as above
+          
+          navigate("/myProfile");
+        }
+      } catch (err) {  //could be an error with users signing in w Google popup. If so, we need to catch this error.
+        console.error("Google Sign-In error:", err);
+        setErrorMessage("Failed to sign in with Google. Please try again.");
+      } finally {
+        setLoading(false); // Reset loading state regardless of if Google sign-in was successful or not
+      }
     }
   };
 
   //Upon successfult sign-in set userLoggedIn from AuthContext to true and set url path to /myProfile
   //This file should render the profile page if userLoggedIn is true
-
 
   return (
     <section className={css["signin-container"]}>
@@ -91,6 +106,8 @@ const SignIn = () => {
           <input type="email" value={email} name="email" placeholder="Email Address" onChange={(e) => handleEmail(e.target.value)} required />
           <input type="password" value={password} name="password" placeholder="Password" onChange={(e) => handlePassword(e.target.value)} required />
           <button type="submit">Sign In</button>
+          Or
+          <button onClick={onGoogleSignIn}>Sign In with Google</button>
         </form>
         <a href="/signUp">Don't have an account? Sign Up</a>
         <a href="/forgot-password">Forgot your password?</a>
