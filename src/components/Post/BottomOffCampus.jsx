ff0bcf56eeda4f823ec77402cafca44ab7639b8c
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2025-01-23 12:14:10 -0500

    Added Bookmark functionality for offCampus and onCampus cards. Everytime a user submits a posting, that posting ID is added to administeredPostings array of that user's doc in firestore

--- src/components/Post/BottomOffCampus.jsx
+++ src/components/Post/BottomOffCampus.jsx
@@ -1,32 +1,103 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAuth } from '../../context/authContext/index';
+import { db } from '../../config/firestore';
+import {collection, updateDoc, getDoc, doc, arrayUnion, arrayRemove} from "firebase/firestore";
 import css from "./styles/BottomPost.module.css";
 
 const BottomOffCampus = ({ members, curNumSeek, address, totalGroupSize, id, listingLocation, onShowMoreClick }) => {
 
+  //destructure currentUser, userLoggedIn from AuthContext
+  const { currentUser, userLoggedIn } = useAuth();
+
+  //state for user document reference
+  const [userRef, setUserRef] = useState(null);
+
+  //state for posting document reference
+  //just for printing purposes
+  const [postingRef, setPostingRef] = useState(doc(db, "postings", id)); //postingRef is initialized to be the doc reference for this particular post
+
+  //Create state variables for this component
   const [bookmarked, setBookmarked] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleBookmarkClick = () => {
-    //If posting not yet bookmarked
-      //Check if user is logged in. If not prompt them to log in and add posting to user's saved postings
-      //Change bookmarked to true to force a page rerender
+  // Set userRef and check initial bookmark state when component mounts or when currentUser changes
+  useEffect(() => {
+    const fetchUserRefAndBookmarkStatus = async () => {
+      if (currentUser) {
+        //update userRef 
+        const userRef = doc(db, "users", currentUser.uid);
+        setUserRef(userRef);
 
-    //If posting is bookmarked
-      //Check if user is logged in. If not prompt them to log in and then remove posting from users saved postings
-      //Change bookmarked to false to force a page rerender
+        //Check if the post is already bookmarked
+        try {
+          const userDoc = await getDoc(userRef);
+          const userBookmarks = userDoc.data()?.bookmarkedPostings || []; //How does this line work?
+          setBookmarked(userBookmarks.includes(id));
+        } catch (err) {
+          console.error("Error fetching user document: ", err);
+        }
+      }
+    };
 
-      console.log(`Post with id, ${id}, was clicked`);
+    fetchUserRefAndBookmarkStatus()
+  }, [currentUser, id]);
 
-  };
-  
-  //TODO: Have bookmark-black.png render if the id of this post is in current user's saved postings. 
+  //Handle bookmark toggle
+  const handleBookmarkClick = async() => {
+
+    console.log(`bookmark for post with id, ${id}, was clicked`);
+
+    //clear previous error message, if any
+    setErrorMessage("");
+
+    //executes if a user is signed in
+    if (currentUser) {
+        try {
+          if (!bookmarked) {
+            // Add the post to the user's bookmarkedPostings array
+            await updateDoc(userRef, {
+              bookmarkedPostings: arrayUnion(id),
+            });
+            console.log("posting", postingRef);
+            console.log("Added to bookmarkedPostings array of user", userRef);
+          } else {
+            // Remove the post from the user's bookmarkedPostings array
+            await updateDoc(userRef, {
+              bookmarkedPostings: arrayRemove(id)
+            });
+            console.log(`Removed posting with id ${id} from bookmarkedPostings of user`, userRef);
+          }
+
+          // Toggle bookmark state for UI
+          setBookmarked(!bookmarked);
+        } catch (err) {
+          console.error("Error updating bookmarkedPostings field: ", err);
+          setErrorMessage("Something went wrong. Please try again.");
+        }
+      } else {
+        // Prompt user to sign in if not logged in
+        setErrorMessage("You need to sign in to bookmark a post.");
+      }
+    };
+
+  //      Have bookmark-black.png render if the id of this post is in current user's saved postings. 
   //      Have bookmark-white.png render if not
   //      If user isn't logged in show bookmark-white.png
 
   return (
     <div className={css.bottom}>
       <div className={css.container}>
-        Bookmark posting 
-        <img className='bookmarkWhite' src='/assets/postings/bookmark-white.png' alt='whiteBookmark'/>
+        Bookmark
+        {errorMessage && <p className={css.errorMessage}> {errorMessage} </p>}
+        {/* The code below renders the white bookmark if a user isn't signed in. If a user is signed in, then the black bookmark will render if bookmarked is true 
+        and the white on will render if not*/}
+        <img
+          onClick={handleBookmarkClick}
+          className={bookmarked ? "bookmark" : "bookmarkWhite"}
+          src={bookmarked ? "/assets/postings/bookmark.png" : "/assets/postings/bookmark-white.png"}
+          alt={bookmarked ? "Bookmark" : "White Bookmark"}
+        />
+        {/*<img className='bookmarkWhite' src='/assets/postings/bookmark-white.png' alt='whiteBookmark'/>*/}
         <span>{totalGroupSize - curNumSeek} looking for {curNumSeek} more</span>
       </div>
       <div className={css.bottomIMGcontainer}>
@@ -36,7 +107,7 @@ const BottomOffCampus = ({ members, curNumSeek, address, totalGroupSize, id, lis
         </div>
         <div className={css.bottomIMG}>
           <img className="bed" src="/assets/postings/bed.png" alt="bed" />
-          <span>{totalGroupSize} People Total</span>
+          <span>{totalGroupSize}-man Housing</span>
         </div>
       </div>
       <div className={`${css.showMore} offCampus`} id={id}>
--- src/components/Post/BottomOnCampus.jsx
+++ src/components/Post/BottomOnCampus.jsx
@@ -1,31 +1,95 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAuth } from '../../context/authContext';
+import { db } from "../../config/firestore";
+import { updateDoc, getDoc, doc, arrayUnion, arrayRemove } from 'firebase/firestore';
 import css from "./styles/BottomPost.module.css";
 
 const BottomOnCampus = ({ members, curNumSeek, dorm, totalGroupSize, id, listingLocation, onShowMoreClick }) => {
   
+  //destructure currentUser from AuthContext
+  const { currentUser } = useAuth();
+
+  //state for user document reference
+  const [userRef, setUserRef] = useState(null);
+
+  //Create state variables for this component
   const [bookmarked, setBookmarked] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // Set userRef and check initial bookmark state when component mounts or when currentUser changes
+  useEffect(() => {
+    const fetchUserRefAndBookmarkStatus = async () => {
+      if (currentUser) {
+        //update userRef
+        const userRef = doc(db, "users", currentUser.uid);
+        setUserRef(userRef);
+
+        //Check if the post is already bookmarked
+        try {
+          const userDoc = await getDoc(userRef);
+          const userBookmarks = userDoc.data()?.bookmarkedPostings || []; //How does this line work?
+          setBookmarked(userBookmarks.includes(id));
+        } catch (err) {
+          console.error("Error fetching user document: ", err)
+        }
+      }
+    };
+
+    fetchUserRefAndBookmarkStatus()
+  }, [currentUser, id]);
   
-  const handleBookmarkClick = () => {
-    //If posting not yet bookmarked
-      //Check if user is logged in. If not prompt them to log in and add posting to user's saved postings
-      //Change bookmarked to true to force a page rerender
+  //Handle bookmark toggle
+  const handleBookmarkClick = async() => {
 
-    //If posting is bookmarked
-      //Check if user is logged in. If not prompt them to log in and then remove posting from users saved postings
-      //Change bookmarked to false to force a page rerender
+    console.log(`bookmark for post with id, ${id}, was clicked`);
 
-      console.log(`Post with id, ${id}, was clicked`);
+    //clear previous error message, if any
+    setErrorMessage("");
 
+    //executes if a user is signed in
+    if (currentUser) {
+      try {
+        if (!bookmarked) {
+          // Add the post to the user's bookmarkedPostings array
+          await updateDoc(userRef, {
+            bookmarkedPostings: arrayUnion(id),
+          });
+          console.log("Added to bookmarkedPostings array of user", userRef);
+        } else {
+          // Remove the post from the user's bookmarkedPostings array
+          await updateDoc(userRef, {
+            bookmarkedPostings: arrayRemove(id)
+          });
+          console.log(`Removed posting with id ${id} from bookmarkedPostings of user`, userRef);
+        }
+
+        //Toggle bookmark state for UI
+        setBookmarked(!bookmarked);
+      } catch (err) {
+        console.error("Error updating bookmarkedPostings field: ", err);
+        setErrorMessage("Something went wrong. Please try again.");
+      }
+    } else {
+      // Prompt user to sign in if not logged in
+      setErrorMessage("You need to sign in to bookmark a post")
+    }
   };
+    
   
-  //TODO: Have bookmark-black.png render if the id of this post is in current user's saved postings. 
+  //      Have bookmark-black.png render if the id of this post is in current user's saved postings. 
   //      Have bookmark-white.png render if not
   //      If user isn't logged in show bookmark-white.png
   return (
     <div className={css.bottom}>
       <div className={css.container}>
-          Bookmark 
-          <img onClick={handleBookmarkClick} className='bookmarkWhite' src='/assets/postings/bookmark-white.png' alt='whiteBookmark'/>
+          Bookmark
+          {errorMessage && <p className={css.errorMessage}>{errorMessage}</p>} 
+          <img
+            onClick={handleBookmarkClick}
+            className={bookmarked ? "bookmark" : "bookmarkWhite"}
+            src={bookmarked ? "/assets/postings/bookmark.png" : "/assets/postings/bookmark-white.png"}
+            alt={bookmarked ? "Bookmark" : "White Bookmark"}
+          />
         <span>{totalGroupSize - curNumSeek} looking for {curNumSeek} more</span>
       </div>
       <div className={css.bottomIMGcontainer}>
